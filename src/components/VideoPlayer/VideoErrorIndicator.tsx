3ad39065e191e335aad81f6a1e67c835ccdfb698
HezekielT <hezekiel.feyissa@gmail.com>
2025-03-02 15:06:37 +0300

    added isPreview prop to VideoErrorIndicator

--- src/components/VideoPlayer/VideoErrorIndicator.tsx
+++ src/components/VideoPlayer/VideoErrorIndicator.tsx
@@ -1,24 +1,36 @@
-
 import React from 'react';
 import {View} from 'react-native';
 import Icon from '@components/Icon';
 import * as Expensicons from '@components/Icon/Expensicons';
+import Text from '@components/Text';
+import useLocalize from '@hooks/useLocalize';
 import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
 import variables from '@styles/variables';
 
-function VideoErrorIndicator() {
+type VideoErrorIndicatorProps = {
+    /** Whether it is a preview or not */
+    isPreview?: boolean;
+};
+
+function VideoErrorIndicator({isPreview = false}: VideoErrorIndicatorProps) {
     const theme = useTheme();
     const styles = useThemeStyles();
+    const {translate} = useLocalize();
 
     return (
-        <View style={[styles.flexColumn, styles.alignItemsCenter, styles.justifyContentCenter, styles.pAbsolute, styles.h100, styles.w100, styles.highlightBG]}>
+        <View style={[styles.flexColumn, styles.alignItemsCenter, styles.justifyContentCenter, styles.pAbsolute, styles.h100, styles.w100]}>
             <Icon
-                fill={theme.activeComponentBG}
+                fill={isPreview ? theme.border : theme.icon}
                 src={Expensicons.VideoSlash}
-                width={variables.iconSizeSuperLarge}
-                height={variables.iconSizeSuperLarge}
+                width={variables.eReceiptEmptyIconWidth}
+                height={variables.eReceiptEmptyIconWidth}
             />
+            {!isPreview && (
+                <View>
+                    <Text style={styles.videoErrorText}>{translate('common.errorOccuredWhileTryingToPlayVideo')}</Text>
+                </View>
+            )}
         </View>
     );
 }
